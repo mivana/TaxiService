379568316ec845e4e124850169e45e95642bd37e
Ivana Markanovic <33691866+mivana@users.noreply.github.com>
2018-12-19 12:21:50 +0100

    Front
    Napravljen Angular projekat

---
+++ initialApp-master/RentApp/Controllers/RideController.cs
@@ -0,0 +1,28 @@
+﻿using RentApp.Models.Entities;
+using RentApp.Persistance.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RentApp.Controllers
+{
+    public class RideController : ApiController
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public RideController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Vehicle
+        public IEnumerable<Ride> GetRides()
+        {
+            return unitOfWork.Rides.GetAll().Where(u => u.Deleted == false);
+        }
+
+    }
+}
---
+++ initialApp-master/RentApp/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using RentApp.Models.Entities;
+using RentApp.Persistance.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace RentApp.Controllers
+{
+    [RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public UserController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        public IEnumerable<AppUser> GetUsers()
+        {
+            return unitOfWork.AppUsers.GetAll().Where(u => u.Deleted == false);
+        }
+
+    }
+}
